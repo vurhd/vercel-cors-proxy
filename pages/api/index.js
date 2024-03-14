70d4f613e39d672157b96d6bed792ecc807f5e14
Dhruv Sachdev <sachdevd@tcd.ie>
2024-03-14 14:43:55 +0100

    allow cors

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,17 @@
 const axios = require('axios');
 
 export default async function handler(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed methods
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allowed headers
+
+  // Handle OPTIONS request (CORS preflight)
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   try {
     const { url, headers, method, body } = req.body;
 
@@ -13,9 +24,7 @@ export default async function handler(req, res) {
 
     const response = await axios(options);
 
-    res.status(response.status);
-    res.setHeader('Content-Type', response.headers['content-type']);
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.status(response.status).setHeader('Content-Type', response.headers['content-type']);
     res.send(response.data);
   } catch (error) {
     console.error(error);
