389eba1784ca8db45eaaef4a37f3c2c69fa3e871
Dhruv Sachdev <sachdevd@tcd.ie>
2024-03-14 14:22:42 +0100

    accept headers

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
+const querystring = require('querystring');
 
 export default async function handler(req, res) {
   try {
-    const { url, headers, method, body } = req.query;
+    const { url, method, body, headers: encodedHeaders } = req.query;
+
+    const headers = querystring.parse(encodedHeaders, ';', '=');
 
     const options = {
       method: method || 'GET',
